d0454e674e69a5c1be7520a7d6d33db38890a0b0
Marco Lopes <mlopes_filho@hotmail.com>
2019-11-19 09:08:44 +0100

    Add project files.

---
+++ CountDownKata.Tests/TestClass.cs
@@ -0,0 +1,29 @@
+﻿// NUnit 3 tests
+// See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using CountDownKata;
+using System;
+using System.Threading;
+
+
+namespace CountDownKata.Tests
+{
+
+[TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void TestMethod()
+        {
+            // TODO: Add your test code here
+            var countDown = new CountDown();
+            countDown.Time = new TimeSpan(10, 14, 15);
+            var millis = countDown.Time.TotalMilliseconds;
+            countDown.StartTime();
+            Thread.Sleep(1000);
+            Assert.That(countDown.Current.TotalMilliseconds, Is.LessThan(millis), "must be less");
+        }
+    }
+}
---
+++ CountDownKata/CountDown.cs
@@ -0,0 +1,71 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CountDownKata
+{
+    public class CountDown : ViewModelBase
+    {
+        private TimeSpan _time;
+        private CancellationTokenSource _cancelationSource;
+
+        public TimeSpan Time
+        {
+            get
+            {
+                return _time;
+            }
+
+            set
+            {
+                Set(ref _time, value);
+            }
+        }
+
+        private TimeSpan _current;
+
+        public TimeSpan Current
+        {
+            get
+            {
+                return _current;
+            }
+
+            set
+            {
+                Set(ref _current, value);
+                RaisePropertyChanged();
+            }
+        }
+
+        public string CounterString
+        {
+            get
+            {
+                return Current.ToString(@"hh\:ss");
+            }
+        }
+
+
+        public void StartTime()
+        {
+           
+            Current = new TimeSpan(0,15,0);
+            if (_cancelationSource == null)
+            {
+                _cancelationSource = new CancellationTokenSource();
+                RunPeriodic(100, _cancelationSource.Token, () => Current = Current.Add(TimeSpan.FromMilliseconds(100) ));
+            }
+        }
+
+        public static async Task RunPeriodic(int milliseconds, CancellationToken token, Action action)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                await Task.Delay(milliseconds, token);
+                action();
+            }
+        }
+    }
+}
